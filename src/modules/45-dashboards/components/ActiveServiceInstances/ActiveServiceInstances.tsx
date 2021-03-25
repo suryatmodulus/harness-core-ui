a082371be56a4a8c3a660cae4bfed23a6d1054fe
Vardan Bansal <vardan.bansal@harness.io>
2021-03-25 16:54:35 +0530

    feat: [CDNG-5425]: WIP

--- src/modules/45-dashboards/components/ActiveServiceInstances/ActiveServiceInstances.tsx
+++ src/modules/45-dashboards/components/ActiveServiceInstances/ActiveServiceInstances.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Layout, Container, Text } from '@wings-software/uicore'
+import { useStrings } from 'framework/exports'
+
+import mockData from './service-details.json'
 
 interface ActiveServiceInstancesProps {}
 
 const ActiveServiceInstances: React.FC<ActiveServiceInstancesProps> = () => {
-  return <></>
+  const { getString } = useStrings()
+  return (
+    <Container width={600} height={600} border={{ color: 'dark' }}>
+      <Layout.Vertical spacing="medium">
+        <Layout.Vertical spacing="medium">
+          <Text font="medium">{getString('serviceDashboard.activeServiceInstancesLabel')}</Text>
+          <Layout.Horizontal spacing="medium">
+            <Text font="large">{mockData.details.total}</Text>
+          </Layout.Horizontal>
+        </Layout.Vertical>
+      </Layout.Vertical>
+    </Container>
+  )
 }
 
 export default ActiveServiceInstances

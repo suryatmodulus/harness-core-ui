5ca2317e97e4184d6cb50f1a3ec063c130563faa
Sahithi Kolichala <sahithi.kolichala@harness.io>
2021-05-06 16:27:17 -0700

    fix: terraform git config dialog

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -1317,6 +1317,7 @@ export interface StringsMap {
   'cd.cdTrialHomePage.startTrial.description': string
   'cd.cdTrialHomePage.startTrial.startBtn.description': string
   'cd.chooseProvisionerText': string
+  'cd.configFilePlaceHolder': string
   'cd.continuous': string
   'cd.createOrSelectProject': string
   'cd.dashboard.createProject': string

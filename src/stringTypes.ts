ff4860bcc3c55fd2b24f69f74d96bb0b5f94bb8f
Sahithi Kolichala <sahithi.kolichala@harness.io>
2021-05-10 16:08:13 -0700

    fix: added flow for inline terraform varfile

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -1313,6 +1313,8 @@ export interface StringsMap {
   'pipeline.triggers.validation.repoName': string
   'pipeline.triggers.validation.triggerName': string
   'pipeline.verificationResult': string
+  'cd.addInline': string
+  'cd.addRemote': string
   'cd.allowSimultaneousDeployments': string
   'cd.cdTrialHomePage.startTrial.description': string
   'cd.cdTrialHomePage.startTrial.startBtn.description': string

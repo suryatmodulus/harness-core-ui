e952ab54fdda88f0ac483cbc50cfcf0b19250ccb
Sahithi Kolichala <sahithi.kolichala@harness.io>
2021-05-14 12:56:19 -0700

    fix: added terraform var widget

--- src/modules/75-cd/components/PipelineSteps/Common/Terraform/Editview/TFRemoteWizard.tsx
+++ src/modules/75-cd/components/PipelineSteps/Common/Terraform/Editview/TFRemoteWizard.tsx
@@ -10,6 +10,9 @@ import {
 } from '@wings-software/uicore'
 import React from 'react'
 import cx from 'classnames'
+import * as Yup from 'yup'
+import { v4 as uuid } from 'uuid'
+
 import { FieldArray, Form } from 'formik'
 
 import { useStrings } from 'framework/strings'
@@ -24,28 +27,47 @@ import { merge } from 'lodash-es'
 
 interface TFRemoteProps {
   onSubmitCallBack: (data: TerraformVarFileWrapper) => void
+  isEditMode: boolean
 }
 export const TFRemoteWizard: React.FC<StepProps<any> & TFRemoteProps> = ({
   previousStep,
   prevStepData,
-  onSubmitCallBack
+  onSubmitCallBack,
+  isEditMode
 }) => {
-  console.log(prevStepData, 'data')
-
   const { getString } = useStrings()
-  const initialValues = {
-    varFile: {
-      type: TerraformStoreTypes.Remote,
-      store: {
-        spec: {
-          gitFetchType: '',
-          branch: '',
-          commitId: '',
-          paths: []
+  console.log(prevStepData, 'data')
+  const initialValues = isEditMode
+    ? {
+        varFile: {
+          identifier: prevStepData?.varFile?.identifier,
+          type: TerraformStoreTypes.Remote,
+          store: {
+            spec: {
+              gitFetchType: prevStepData?.varFile?.store?.spec?.gitFetchType,
+              branch: prevStepData?.varFile?.store?.spec?.branch,
+              commitId: prevStepData?.varFile?.store?.spec?.commitId,
+              paths: (prevStepData?.varFile?.store?.spec?.paths || []).map((item: string) => ({
+                path: item,
+                id: uuid()
+              }))
+            }
+          }
         }
       }
-    }
-  }
+    : {
+        varFile: {
+          type: TerraformStoreTypes.Remote,
+          store: {
+            spec: {
+              gitFetchType: '',
+              branch: '',
+              commitId: '',
+              paths: []
+            }
+          }
+        }
+      }
   const { expressions } = useVariablesExpression()
 
   const gitFetchTypes: SelectOption[] = [
@@ -58,14 +80,60 @@ export const TFRemoteWizard: React.FC<StepProps<any> & TFRemoteProps> = ({
         initialValues={initialValues}
         onSubmit={values => {
           const payload = merge(prevStepData, values)
-          onSubmitCallBack(payload)
+
+          const data = {
+            varFile: {
+              type: payload.varFile.type,
+              identifier: payload.varFile.identifier,
+              store: {
+                ...payload.varFile.store,
+                type: payload?.varFile?.store?.spec?.connectorRef?.connector?.type,
+                spec: {
+                  ...payload.varFile.store?.spec,
+                  connectorRef: payload?.varFile?.store?.spec?.connectorRef
+                    ? getMultiTypeFromValue(payload?.varFile?.store?.spec?.connectorRef) === MultiTypeInputType.RUNTIME
+                      ? payload?.varFile?.store?.spec?.connectorRef
+                      : payload?.varFile?.store?.spec?.connectorRef?.value
+                    : ''
+                }
+              }
+            }
+          }
+          if (payload.varFile.store?.spec?.paths?.length) {
+            data.varFile.store.spec['paths'] = payload.varFile.store?.spec?.paths?.map(
+              (item: PathInterface) => item.path
+            ) as any
+          }
+          console.log(data, 'data')
+          onSubmitCallBack(data)
         }}
+        validationSchema={Yup.object().shape({
+          varFile: Yup.object().shape({
+            identifier: Yup.string().required(getString('common.validation.identifierIsRequired')),
+            store: Yup.object().shape({
+              spec: Yup.object().shape({
+                gitFetchType: Yup.string().required(getString('cd.gitFetchTypeRequired')),
+                branch: Yup.string().when('gitFetchType', {
+                  is: 'Branch',
+                  then: Yup.string().trim().required(getString('validation.branchName'))
+                }),
+                commitId: Yup.string().when('gitFetchType', {
+                  is: 'Commit',
+                  then: Yup.string().trim().required(getString('validation.commitId'))
+                }),
+                paths: Yup.string().required(getString('cd.pathCannotBeEmpty'))
+              })
+            })
+          })
+        })}
       >
         {formik => {
           return (
             <Form>
               <div className={css.tfRemoteForm}>
-                <FormInput.Text name="varFile.identifier" label={getString('cd.fileIdentifier')} />
+                <div className={cx(stepCss.formGroup, stepCss.md)}>
+                  <FormInput.Text name="varFile.identifier" label={getString('cd.fileIdentifier')} />
+                </div>
                 <div className={cx(stepCss.formGroup, stepCss.md)}>
                   <FormInput.Select
                     items={gitFetchTypes}

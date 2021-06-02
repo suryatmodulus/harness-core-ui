a4816ffdf2813f9436e32ffbaf6c0328d753ac9e
deepesh-ui <deepesh.kumar@harness.io>
2021-06-02 07:48:18 +0530

    fix: (CVNG-2741) loding for sumologic connector url dropdown

--- src/modules/35-connectors/components/CreateConnector/SumoLogicConnector/CreateSumoLogicConnector.tsx
+++ src/modules/35-connectors/components/CreateConnector/SumoLogicConnector/CreateSumoLogicConnector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import * as Yup from 'yup'
 import { Container, FormikForm, Layout, FormInput, Formik, Button, SelectOption } from '@wings-software/uicore'
 import { buildSumoLogicPayload } from '@connectors/pages/connectors/utils/ConnectorUtils'
@@ -6,6 +6,7 @@ import SecretInput from '@secrets/components/SecretInput/SecretInput'
 import type { ConnectorConfigDTO } from 'services/cd-ng'
 import { useStrings } from 'framework/strings'
 import { Connectors } from '@connectors/constants'
+import { useToaster } from '@common/exports'
 import { useGetSumoLogicEndPoints } from 'services/cv'
 import { PageSpinner } from '@common/components/Page/PageSpinner'
 import { cvConnectorHOC } from '../CommonCVConnector/CVConnectorHOC'
@@ -15,10 +16,11 @@ import { StepDetailsHeader } from '../CommonCVConnector/CredentialsStepHeader'
 import css from './CreateSumoLogicConnector.module.scss'
 
 export function SumoLogicConfigStep(props: ConnectionConfigProps): JSX.Element {
-  const { nextStep, prevStepData, connectorInfo, projectIdentifier, orgIdentifier, accountId } = props
+  const { nextStep, prevStepData, connectorInfo, projectIdentifier, orgIdentifier, accountId, isEditMode } = props
   const { getString } = useStrings()
-  const { data: endPoints, loading: loadingEndpoints } = useGetSumoLogicEndPoints({})
-  const [urlList, updateUrlList] = useState<SelectOption[]>([])
+  const { showError, clear } = useToaster()
+  const { data: endPoints, error: endPointError, loading: loadingEndpoints } = useGetSumoLogicEndPoints({})
+
   const [initialValues, setInitialValues] = useState<ConnectorConfigDTO>({
     url: '',
     apiKeyRef: {},
@@ -30,20 +32,34 @@ export function SumoLogicConfigStep(props: ConnectionConfigProps): JSX.Element {
   })
 
   useEffect(() => {
-    const filteredPoints: SelectOption[] = []
-    for (const endPoint of endPoints?.data || []) {
-      endPoint && filteredPoints.push({ label: endPoint, value: endPoint })
-    }
-    updateUrlList(filteredPoints)
-  }, [endPoints, loadingEndpoints])
-
-  useEffect(() => {
     async function updateStepData(): Promise<void> {
       const value = await initializeSumoLogicConnectorWithStepData(prevStepData, accountId)
       value && setInitialValues(value)
     }
     updateStepData()
   }, [prevStepData, accountId])
+
+  const endPointOptions = useMemo((): SelectOption[] => {
+    if (loadingEndpoints) {
+      return [{ label: getString('loading'), value: '' }]
+    } else if (endPointError?.message) {
+      clear()
+      showError(endPointError?.message)
+      return []
+    }
+    const filteredPoints: SelectOption[] = []
+    for (const endPoint of endPoints?.data || []) {
+      if (endPoint) {
+        filteredPoints.push({ label: endPoint, value: endPoint })
+      }
+    }
+
+    // set default value
+    if (!isEditMode && !initialValues.url) {
+      setInitialValues({ ...initialValues, url: filteredPoints[0].label })
+    }
+    return filteredPoints
+  }, [endPoints, endPointError, loadingEndpoints])
 
   if (initialValues?.loading) {
     return <PageSpinner />
@@ -64,11 +80,7 @@ export function SumoLogicConfigStep(props: ConnectionConfigProps): JSX.Element {
       >
         <FormikForm className={css.form}>
           <Layout.Vertical spacing="large" height={450}>
-            <FormInput.Select
-              items={loadingEndpoints ? [{ label: 'loading', value: '' }] : urlList}
-              label={getString('connectors.sumoLogic.urlLabel')}
-              name="url"
-            />
+            <FormInput.Select items={endPointOptions} label={getString('connectors.sumoLogic.urlLabel')} name="url" />
             <SecretInput label={getString('connectors.sumoLogic.encryptedAccessIdLabel')} name="accessIdRef" />
             <SecretInput label={getString('connectors.sumoLogic.encryptedAccessKeyLabel')} name="accesskeyRef" />
           </Layout.Vertical>

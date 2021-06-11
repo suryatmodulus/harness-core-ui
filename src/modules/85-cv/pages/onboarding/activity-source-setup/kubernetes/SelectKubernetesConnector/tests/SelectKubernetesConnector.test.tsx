c5c5ef84b5b0d1b2da04ed115d2844b0a40fb2e5
deepesh-ui <deepesh.kumar@harness.io>
2021-06-11 14:38:12 +0530

    fix: (CVNG-1429)) testcase for validating connector error

--- src/modules/85-cv/pages/onboarding/activity-source-setup/kubernetes/SelectKubernetesConnector/tests/SelectKubernetesConnector.test.tsx
+++ src/modules/85-cv/pages/onboarding/activity-source-setup/kubernetes/SelectKubernetesConnector/tests/SelectKubernetesConnector.test.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { fireEvent, render, waitFor } from '@testing-library/react'
 import { Container, FormInput } from '@wings-software/uicore'
 import { TestWrapper } from '@common/utils/testUtils'
+import * as CVService from 'services/cv'
+import * as toaster from '@common/components/Toaster/useToaster'
 import routes from '@common/RouteDefinitions'
 import { accountPathProps, projectPathProps } from '@common/utils/routeUtils'
 import { SelectKubernetesConnector } from '../SelectKubernetesConnector'
@@ -12,6 +14,13 @@ const MockConnectorObj = {
     name: 'connector'
   }
 }
+
+const STATUS = {
+  ERROR: 'ERROR',
+  SUCCESS: 'SUCCESS'
+}
+
+const errorConnectorValidation = 'No Delegates found'
 
 jest.mock('@cv/pages/onboarding/SelectOrCreateConnector/SelectOrCreateConnector', () => ({
   ...(jest.requireActual('@cv/pages/onboarding/SelectOrCreateConnector/SelectOrCreateConnector') as any),
@@ -27,6 +36,9 @@ jest.mock('@cv/pages/onboarding/SelectOrCreateConnector/SelectOrCreateConnector'
 
 describe('Unit tests for SelectActivitySource', () => {
   test('Ensure validation works', async () => {
+    jest.spyOn(CVService, 'validateConnectorPromise').mockResolvedValue({
+      status: STATUS.SUCCESS
+    } as any)
     const onSubmitMockFunc = jest.fn()
     const { container } = render(
       <TestWrapper
@@ -72,5 +84,51 @@ describe('Unit tests for SelectActivitySource', () => {
         selectedWorkloads: new Map()
       })
     )
+  })
+
+  test('Ensure user is not able to move to Choose Namespace Section if connector validation fails', async () => {
+    jest.spyOn(CVService, 'validateConnectorPromise').mockResolvedValue({
+      status: STATUS.ERROR,
+      message: errorConnectorValidation
+    } as any)
+    const showError = jest.fn()
+    const useToasterSpy = jest.spyOn(toaster, 'useToaster')
+    useToasterSpy.mockImplementation(
+      () =>
+        ({
+          showError
+        } as any)
+    )
+    const onSubmitMockFunc = jest.fn()
+    const { container } = render(
+      <TestWrapper
+        path={routes.toCVProjectOverview({ ...accountPathProps, ...projectPathProps })}
+        pathParams={{
+          accountId: '1234_accountId',
+          projectIdentifier: '1234_project',
+          orgIdentifier: '1234_ORG'
+        }}
+      >
+        <SelectKubernetesConnector onSubmit={onSubmitMockFunc} onPrevious={() => undefined} />
+      </TestWrapper>
+    )
+
+    await waitFor(() => expect(container.querySelector('[class*="main"]')).not.toBeNull())
+
+    const mockInputButton = container.querySelector('[class*="mockInput"] button')
+    if (!mockInputButton) {
+      throw Error('Mock input button was not rendered')
+    }
+
+    fireEvent.click(mockInputButton)
+    await waitFor(() => undefined)
+
+    const submitButton = container.querySelector('button[type="submit"]')
+    if (!submitButton) {
+      throw Error('Submit button was not rendered.')
+    }
+    fireEvent.click(submitButton)
+    await waitFor(() => expect(showError).toHaveBeenCalledWith(errorConnectorValidation))
+    await waitFor(() => expect(onSubmitMockFunc).not.toHaveBeenCalled())
   })
 })

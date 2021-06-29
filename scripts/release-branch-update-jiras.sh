9bedefbf2e2591ed6fbfbec0cf219c532eeeaa61
swarajcheguri-harness <swaraj.cheguri@harness.io>
2021-06-29 11:18:29 +0530

    with working script

--- scripts/release-branch-update-jiras.sh
+++ scripts/release-branch-update-jiras.sh
@@ -1,7 +1,6 @@
 # ------ from Commit List => extract JIRA Tickets
 # [(CCM|CD|CE|DOC|ER|HAR|LE|PL|SEC|SWAT|UI|DX)-[0-9]+]
-
-KEYS=`git log --pretty=oneline --abbrev-commit | awk "1;/update/{exit}" | grep -o -iE '(CCM|CE|CD|CDC|CDP|CE|CI|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|UI|DX|DEL|CDNG)-[0-9]+' | sort | uniq`
+KEYS=`git log --pretty=oneline --abbrev-commit | awk "1;/Branching to release\/{exit}" | grep -o -iE '(CCM|CE|CD|CDC|CDP|CE|CI|CV|DOC|ER|HAR|LE|PL|SEC|SWAT|UI|DX|DEL|CDNG)-[0-9]+' | sort | uniq`
 echo --- List of JIRA ---
 echo $KEYS
 echo --- End

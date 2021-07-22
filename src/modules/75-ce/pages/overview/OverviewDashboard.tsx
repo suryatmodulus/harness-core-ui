96809ac49681e91b9907ae20de7f98a36c326331
Rishabh Kumar <rishabh.kumar@harness.io>
2021-07-22 23:00:21 +0530

    chore: integrated gql api for overview and perspective screens

--- src/modules/75-ce/pages/overview/OverviewDashboard.tsx
+++ src/modules/75-ce/pages/overview/OverviewDashboard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useCreateConnectorMinimal } from '@ce/components/CreateConnector/CreateConnector'
+import { CcmMetaData, useCcmMetaDataQuery } from 'services/ce/services'
+import { Utils } from '@ce/common/Utils'
 import bgImage from './images/CD/overviewBg.png'
 
 // interface OverviewDashboardProps {}
@@ -8,9 +10,12 @@ import bgImage from './images/CD/overviewBg.png'
 const OverviewDashboard: React.FC = () => {
   const { openModal } = useCreateConnectorMinimal()
 
+  const [result] = useCcmMetaDataQuery()
+  const { data } = result
+
   useEffect(() => {
-    openModal()
-  }, [])
+    if (data && !Utils.accountHasConnectors(data.ccmMetaData as CcmMetaData)) openModal()
+  }, [data])
 
   return (
     <div style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', height: '100%', width: '100%' }}></div>
--- src/modules/75-ce/pages/perspective/PerspectiveDashboard.tsx
+++ src/modules/75-ce/pages/perspective/PerspectiveDashboard.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { useCreateConnectorMinimal } from '@ce/components/CreateConnector/CreateConnector'
+import { CcmMetaData, useCcmMetaDataQuery } from 'services/ce/services'
+import { Utils } from '@ce/common/Utils'
 import bgImage from './images/perspectiveBg.png'
 
 // interface PerspectiveDashboardProps {}
@@ -7,9 +9,12 @@ import bgImage from './images/perspectiveBg.png'
 const PerspectiveDashboard: React.FC = () => {
   const { openModal } = useCreateConnectorMinimal()
 
+  const [result] = useCcmMetaDataQuery()
+  const { data } = result
+
   useEffect(() => {
-    openModal()
-  }, [])
+    if (data && !Utils.accountHasConnectors(data.ccmMetaData as CcmMetaData)) openModal()
+  }, [data])
 
   return (
     <div style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', height: '100%', width: '100%' }}></div>

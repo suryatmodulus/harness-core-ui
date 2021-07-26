208bb30a384acdedd7a9d8ba712c9477aef059a7
Sahithi Kolichala <sahithi.kolichala@harness.io>
2021-07-26 14:21:09 -0700

    feat: moved manifestInput set form to a separate component

---
+++ src/modules/70-pipeline/components/ManifestInputForm/ManifestInputForm.module.scss.d.ts
@@ -0,0 +1,41 @@
+/* eslint-disable */
+// this is an auto-generated file
+declare const styles: {
+  readonly disabled: string
+  readonly sectionHeader: string
+  readonly subSectonHeader: string
+  readonly stagesTreeBulletCircle: string
+  readonly artifactHeader: string
+  readonly mtop: string
+  readonly customVariables: string
+  readonly padSmall: string
+  readonly inputWidth: string
+  readonly nestedAccordions: string
+  readonly nopadLeft: string
+  readonly selectPopover: string
+  readonly tabHeading: string
+  readonly tabSubHeading: string
+  readonly sectionCard: string
+  readonly shadow: string
+  readonly notwide: string
+  readonly accordionTitle: string
+  readonly cardSection: string
+  readonly serviceDefinition: string
+  readonly accordionSummary: string
+  readonly header: string
+  readonly subheading: string
+  readonly inputheader: string
+  readonly artifactInfoTooltip: string
+  readonly manifestVariablesTable: string
+  readonly skipVersioning: string
+  readonly width50: string
+  readonly width25: string
+  readonly serviceSection: string
+  readonly connectorMargin: string
+  readonly layoutVerticalSpacing: string
+  readonly verticalSpacingInput: string
+  readonly listLabel: string
+  readonly width400: string
+  readonly artifactsAccordion: string
+}
+export default styles

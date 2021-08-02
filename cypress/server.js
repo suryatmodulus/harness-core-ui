63982c7e04c63910b5ec7b3c42eea2eeefc2c7bb
Kapil Kumawat <kapil.kumawat@harness.io>
2021-08-02 21:30:19 +0530

    feat: Base framework

--- cypress/server.js
+++ cypress/server.js
@@ -6,7 +6,41 @@ const https = require('https')
 const app = express()
 const PORT = 8080
 const server = https.createServer({ key: key, cert: cert }, app)
-app.get('*', (req, res) => res.send('Express + TypeScript Server'))
+
+function checkFileExistsSync(filepath) {
+  let flag = true
+  try {
+    fs.accessSync(filepath, fs.constants.F_OK)
+  } catch (e) {
+    flag = false
+  }
+  console.log(filepath, flag)
+  return flag
+}
+
+function getFile(req) {
+  return `./cypress/fixtures${req.url.split('?')[0]}${req.method === 'POST' ? '.post' : ''}.json`
+}
+
+app.all('*', (req, res) => {
+  if (req.get('Content-Type') === 'text/plain') {
+    res.setHeader('Content-Type', 'text/plain')
+  } else {
+    res.setHeader('Content-Type', 'application/json')
+  }
+  const file = getFile(req)
+  if (['POST', 'GET', 'PUT'].indexOf(req.method) > -1 && checkFileExistsSync(file)) {
+    res.end(fs.readFileSync(file))
+  } else {
+    res.end(
+      JSON.stringify({
+        correlationId: '',
+        data: {},
+        status: 'SUCCESS'
+      })
+    )
+  }
+})
 server.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
 })

9e83edcdb94a5d8741a01caa8295f46b3ed0788c
Max <max.gorodov@harness.io>
2021-09-22 13:26:10 +0500

    feat: [CI-2381]: Build execution view - Pipeline header changes

--- src/modules/75-ci/pages/build/sections/commits/BuildCommits.tsx
+++ src/modules/75-ci/pages/build/sections/commits/BuildCommits.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import copy from 'copy-to-clipboard'
 import moment from 'moment'
 import { get } from 'lodash-es'
-import { Card, Container, Text, Icon, Avatar, Color } from '@wings-software/uicore'
+import { Card, Container, Text, Icon, Avatar, Color, Accordion } from '@wings-software/uicore'
 import { useToaster } from '@common/exports'
 import { useExecutionContext } from '@pipeline/context/ExecutionContext'
 import { getShortCommitId, getTimeAgo } from '@ci/services/CIUtils'
-import type { CIBuildCommit, CIWebhookInfoDTO } from 'services/ci'
+import type { CIBuildCommit, CIWebhookInfoDTO, CIPipelineModuleInfo } from 'services/ci'
 import { useStrings } from 'framework/strings'
 import css from './BuildCommits.module.scss'
 
@@ -28,89 +28,182 @@ const AVATAR_COLORS = [
   Color.RED_600
 ]
 
-const BuildCommits: React.FC = () => {
+const Commits: React.FC<{ commits: CIBuildCommit[] }> = ({ commits }): any => {
   const context = useExecutionContext()
+
   const { showSuccess, showError } = useToaster()
   const { getString } = useStrings()
+
   const copy2Clipboard = (text: string): void => {
     copy(String(text))
       ? showSuccess(getString('ci.clipboardCopySuccess'))
       : showError(getString('ci.clipboardCopyFail'), undefined, 'ci.copy.commit.error')
   }
-  const commitsGroupedByTimestamp: CommitsGroupedByTimestamp[] = []
+
   const ciData = get(
     context,
     'pipelineExecutionDetail.pipelineExecutionSummary.moduleInfo.ci.ciExecutionInfoDTO'
   ) as CIWebhookInfoDTO
-  const buildCommits = ciData?.branch?.commits?.length ? ciData.branch.commits : ciData?.pullRequest?.commits
   const commitAuthor = ciData?.author
-  buildCommits?.forEach((commit: CIBuildCommit) => {
-    const index = commitsGroupedByTimestamp.findIndex(({ timeStamp: timestamp2 }) =>
+
+  return commits.map(({ id = '', message = '', timeStamp: commitTimestamp = 0, ownerId, ownerName }, index) => {
+    const [title, description] = message.split('\n\n')
+    // we should use only first part of a name
+    // in order to show a single letter
+    const firstName = (ownerName || commitAuthor!.name)?.split(' ')[0]
+    return (
+      <Card className={css.commit} key={id}>
+        <div>
+          <Text className={css.commitTitle} font="small">
+            {title}
+          </Text>
+          {description && (
+            <Text className={css.commitDescription} font="small" margin={{ top: 'xsmall' }}>
+              {description}
+            </Text>
+          )}
+        </div>
+        <Container flex>
+          <Avatar
+            className={css.avatar}
+            name={firstName}
+            size={'xsmall'}
+            backgroundColor={AVATAR_COLORS[index % AVATAR_COLORS.length]}
+            src={commitAuthor!.avatar}
+            hoverCard={false}
+          />
+          <Text className={css.committed} font="xsmall" margin={{ right: 'xlarge' }}>
+            {ownerId || commitAuthor!.id} {getString('ci.committed')} {getTimeAgo(commitTimestamp)}
+          </Text>
+          <button className={css.hash} onClick={() => copy2Clipboard(id)}>
+            <Text
+              className={css.hashText}
+              font={{ size: 'xsmall', weight: 'semi-bold' }}
+              padding={{ top: 'xsmall', right: 'small', bottom: 'xsmall', left: 'small' }}
+              margin={{ right: 'small' }}
+            >
+              {getShortCommitId(id)}
+            </Text>
+            <Icon className={css.hashIcon} name="clipboard" size={14} margin={{ right: 'small' }} />
+          </button>
+        </Container>
+      </Card>
+    )
+  })
+}
+
+const BuildCommits: React.FC = () => {
+  const context = useExecutionContext()
+  const { getString } = useStrings()
+
+  const ciData = get(context, 'pipelineExecutionDetail.pipelineExecutionSummary.moduleInfo.ci') as CIPipelineModuleInfo
+
+  const codebaseCommits = ciData?.ciExecutionInfoDTO?.branch?.commits?.length
+    ? ciData.ciExecutionInfoDTO.branch.commits
+    : ciData?.ciExecutionInfoDTO?.pullRequest?.commits
+  const triggerCommits = ciData?.ciExecutionInfoDTO?.branch?.triggerCommits?.length
+    ? ciData.ciExecutionInfoDTO.branch.triggerCommits
+    : ciData?.ciExecutionInfoDTO?.pullRequest?.triggerCommits
+
+  const codebaseCommitsGroupedByTimestamp: CommitsGroupedByTimestamp[] = []
+  codebaseCommits?.forEach((commit: CIBuildCommit) => {
+    const index = codebaseCommitsGroupedByTimestamp.findIndex(({ timeStamp: timestamp2 }) =>
       moment(commit.timeStamp).isSame(timestamp2, 'day')
     )
 
     if (index > -1) {
-      commitsGroupedByTimestamp[index].commits.push(commit)
+      codebaseCommitsGroupedByTimestamp[index].commits.push(commit)
     } else {
-      commitsGroupedByTimestamp.push({ timeStamp: commit.timeStamp || 0, commits: [commit] })
+      codebaseCommitsGroupedByTimestamp.push({ timeStamp: commit.timeStamp || 0, commits: [commit] })
     }
   })
+
+  const triggerCommitsGroupedByTimestamp: CommitsGroupedByTimestamp[] = []
+  triggerCommits?.forEach((commit: CIBuildCommit) => {
+    const index = triggerCommitsGroupedByTimestamp.findIndex(({ timeStamp: timestamp2 }) =>
+      moment(commit.timeStamp).isSame(timestamp2, 'day')
+    )
+
+    if (index > -1) {
+      triggerCommitsGroupedByTimestamp[index].commits.push(commit)
+    } else {
+      triggerCommitsGroupedByTimestamp.push({ timeStamp: commit.timeStamp || 0, commits: [commit] })
+    }
+  })
+
   return (
     <div className={css.wrapper}>
-      {commitsGroupedByTimestamp.map(({ timeStamp, commits }) => (
-        <div className={css.stack} key={String(timeStamp)}>
-          <div className={css.stackHeader}>
-            <Icon className={css.stackHeaderIcon} name="git-commit" size={20} margin={{ right: 'medium' }} />
-            <Text className={css.stackHeaderText} font="small">
-              {getString('ci.commitsOn')} {moment(timeStamp).format('MMM D, YYYY')}
-            </Text>
-          </div>
-          {commits.map(({ id = '', message = '', timeStamp: commitTimestamp = 0, ownerId, ownerName }, index) => {
-            const [title, description] = message.split('\n\n')
-            // we should use only first part of a name
-            // in order to show a single letter
-            const firstName = (ownerName || commitAuthor!.name)?.split(' ')[0]
-            return (
-              <Card className={css.commit} key={id}>
-                <div>
-                  <Text className={css.commitTitle} font="small">
-                    {title}
-                  </Text>
-                  {description && (
-                    <Text className={css.commitDescription} font="small" margin={{ top: 'xsmall' }}>
-                      {description}
-                    </Text>
-                  )}
-                </div>
-                <Container flex>
-                  <Avatar
-                    className={css.avatar}
-                    name={firstName}
-                    size={'xsmall'}
-                    backgroundColor={AVATAR_COLORS[index % AVATAR_COLORS.length]}
-                    src={commitAuthor!.avatar}
-                    hoverCard={false}
-                  />
-                  <Text className={css.committed} font="xsmall" margin={{ right: 'xlarge' }}>
-                    {ownerId || commitAuthor!.id} {getString('ci.committed')} {getTimeAgo(commitTimestamp)}
-                  </Text>
-                  <button className={css.hash} onClick={() => copy2Clipboard(id)}>
-                    <Text
-                      className={css.hashText}
-                      font={{ size: 'xsmall', weight: 'semi-bold' }}
-                      padding={{ top: 'xsmall', right: 'small', bottom: 'xsmall', left: 'small' }}
-                      margin={{ right: 'small' }}
-                    >
-                      {getShortCommitId(id)}
-                    </Text>
-                    <Icon className={css.hashIcon} name="clipboard" size={14} margin={{ right: 'small' }} />
-                  </button>
-                </Container>
-              </Card>
-            )
-          })}
-        </div>
-      ))}
+      {triggerCommits?.length > 0 ? (
+        <>
+          <Accordion activeId="codebase-commits">
+            <Accordion.Panel
+              id="codebase-commits"
+              summary={`${ciData?.repoName} (Codebase)`}
+              details={
+                <>
+                  {codebaseCommitsGroupedByTimestamp.map(({ timeStamp, commits }) => (
+                    <div className={css.stack} key={String(timeStamp)}>
+                      <div className={css.stackHeader}>
+                        <Icon
+                          className={css.stackHeaderIcon}
+                          name="git-commit"
+                          size={20}
+                          margin={{ right: 'medium' }}
+                        />
+                        <Text className={css.stackHeaderText} font="small">
+                          {getString('ci.commitsOn')} {moment(timeStamp).format('MMM D, YYYY')}
+                        </Text>
+                      </div>
+                      <Commits commits={commits} />
+                    </div>
+                  ))}
+                </>
+              }
+            />
+          </Accordion>
+
+          <Accordion activeId="trigger-commits">
+            <Accordion.Panel
+              id="trigger-commits"
+              summary={`${ciData?.triggerRepoName} (Trigger)`}
+              details={
+                <>
+                  {triggerCommitsGroupedByTimestamp.map(({ timeStamp, commits }) => (
+                    <div className={css.stack} key={String(timeStamp)}>
+                      <div className={css.stackHeader}>
+                        <Icon
+                          className={css.stackHeaderIcon}
+                          name="git-commit"
+                          size={20}
+                          margin={{ right: 'medium' }}
+                        />
+                        <Text className={css.stackHeaderText} font="small">
+                          {getString('ci.commitsOn')} {moment(timeStamp).format('MMM D, YYYY')}
+                        </Text>
+                      </div>
+                      <Commits commits={commits} />
+                    </div>
+                  ))}
+                </>
+              }
+            />
+          </Accordion>
+        </>
+      ) : (
+        <>
+          {codebaseCommitsGroupedByTimestamp.map(({ timeStamp, commits }) => (
+            <div className={css.stack} key={String(timeStamp)}>
+              <div className={css.stackHeader}>
+                <Icon className={css.stackHeaderIcon} name="git-commit" size={20} margin={{ right: 'medium' }} />
+                <Text className={css.stackHeaderText} font="small">
+                  {getString('ci.commitsOn')} {moment(timeStamp).format('MMM D, YYYY')}
+                </Text>
+              </div>
+              <Commits commits={commits} />
+            </div>
+          ))}
+        </>
+      )}
     </div>
   )
 }

df81b1a313ee6453fb47788ef95e0f25f2d856ca
sunnykesh-harness <sunnykesh.shandilya@harness.io>
2021-10-12 16:13:12 +0530

    Added page reload as retry on API failure

--- src/modules/45-projects-orgs/components/DashboardAPIErrorWidget/DashboardAPIErrorWidget.tsx
+++ src/modules/45-projects-orgs/components/DashboardAPIErrorWidget/DashboardAPIErrorWidget.tsx
@@ -9,20 +9,40 @@ interface DashboardAPIErrorWidgetProps {
   callback?: (options?: Record<string, any>) => Promise<void>
 }
 
-const renderRetryLinks = (callback?: DashboardAPIErrorWidgetProps.callback): JSX.Element => {
-  return <Text>{`Suggestions: Retry or refresh your page.`}</Text>
+const renderRetryLinks = (label: string, callback?: DashboardAPIErrorWidgetProps['callback']): JSX.Element => {
+  return (
+    <Text color={Color.PRIMARY_7} onClick={callback ?? (() => location.reload())}>
+      {label}
+    </Text>
+  )
 }
 
 const DashboardAPIErrorWidget: React.FC<DashboardAPIErrorWidgetProps> = props => {
   const { getString } = useStrings()
 
+  const getSuggestionText = (): JSX.Element => {
+    return (
+      <Layout.Horizontal>
+        <Text padding="xsmall">{'Suggestions: '}</Text>
+        <Text padding="xsmall">{renderRetryLinks('Retry', props.callback)}</Text>
+        <Text padding={{ top: 'xsmall' }}>{'or'}</Text>
+        <Text padding="xsmall">{renderRetryLinks('refresh your page.')}</Text>
+      </Layout.Horizontal>
+    )
+  }
+
   return (
     <Layout.Vertical className={props.className} background={Color.YELLOW_50} flex={{ justifyContent: 'center' }}>
       <Icon name="data-fetch-error" size={120} {...props.iconProps} margin={{ bottom: 'large' }}></Icon>
-      <Text icon="warning-sign" iconProps={{ color: Color.ORANGE_700 }} color={Color.ORANGE_700}>
+      <Text
+        icon="warning-sign"
+        iconProps={{ color: Color.ORANGE_700 }}
+        color={Color.ORANGE_700}
+        font={{ size: 'medium' }}
+      >
         {'Unable to fetch data due to a network error.'}
       </Text>
-      {renderRetryLinks(props.callback)}
+      {getSuggestionText()}
     </Layout.Vertical>
   )
 }

c8873b14ae581eea80795646ccb9e19e561fd0ca
Rishabh Kumar <rishabh.kumar@harness.io>
2021-10-18 13:04:24 +0530

    chore: refactoring

--- src/modules/75-ce/components/COGatewayConfig/steps/ManageResources/DisplaySelectedInstances.tsx
+++ src/modules/75-ce/components/COGatewayConfig/steps/ManageResources/DisplaySelectedInstances.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import type { CellProps } from 'react-table'
+import React, { useMemo } from 'react'
+import type { CellProps, Column } from 'react-table'
 import { Button, Table } from '@wings-software/uicore'
 import type { InstanceDetails } from '@ce/components/COCreateGateway/models'
 import { NameCell, TableCell } from './common'
@@ -14,64 +14,62 @@ export const DisplaySelectedInstances: React.FC<DisplaySelectedInstancesProps> =
   const RemoveCell = (tableProps: CellProps<InstanceDetails>) => {
     return <Button className={css.clearBtn} icon={'delete'} onClick={() => props.onDelete(tableProps.row.index)} />
   }
-  return (
-    <Table<InstanceDetails>
-      data={props.data}
-      bpTableProps={{}}
-      className={css.instanceTable}
-      columns={[
-        {
-          accessor: 'name',
-          Header: 'NAME AND ID',
-          width: '16.5%',
-          Cell: NameCell
-        },
-        {
-          accessor: 'ipv4',
-          Header: 'IP ADDRESS',
-          width: '16.5%',
-          Cell: TableCell,
-          disableSortBy: true
-        },
-        {
-          accessor: 'region',
-          Header: 'REGION',
-          width: '16.5%',
-          Cell: TableCell
-        },
-        {
-          accessor: 'type',
-          Header: 'TYPE',
-          width: '16.5%',
-          Cell: TableCell
-        },
-        {
-          accessor: 'tags',
-          Header: 'TAGS',
-          width: '16.5%',
-          Cell: TableCell
-        },
-        {
-          accessor: 'launch_time',
-          Header: 'LAUNCH TIME',
-          width: '16.5%',
-          Cell: TableCell
-        },
-        {
-          accessor: 'status',
-          Header: 'STATUS',
-          width: '16.5%',
-          Cell: TableCell
-        },
-        {
-          Header: '',
-          id: 'menu',
-          accessor: row => row.id,
-          width: '5%',
-          Cell: RemoveCell,
-          disableSortBy: true
-        }
-      ]}
-    />
+
+  const columns: Array<Column<InstanceDetails>> = useMemo(
+    () => [
+      {
+        accessor: 'name',
+        Header: 'NAME AND ID',
+        width: '16.5%',
+        Cell: NameCell
+      },
+      {
+        accessor: 'ipv4',
+        Header: 'IP ADDRESS',
+        width: '16.5%',
+        Cell: TableCell,
+        disableSortBy: true
+      },
+      {
+        accessor: 'region',
+        Header: 'REGION',
+        width: '16.5%',
+        Cell: TableCell
+      },
+      {
+        accessor: 'type',
+        Header: 'TYPE',
+        width: '16.5%',
+        Cell: TableCell
+      },
+      {
+        accessor: 'tags',
+        Header: 'TAGS',
+        width: '16.5%',
+        Cell: TableCell
+      },
+      {
+        accessor: 'launch_time',
+        Header: 'LAUNCH TIME',
+        width: '16.5%',
+        Cell: TableCell
+      },
+      {
+        accessor: 'status',
+        Header: 'STATUS',
+        width: '16.5%',
+        Cell: TableCell
+      },
+      {
+        Header: '',
+        id: 'menu',
+        accessor: row => row.id,
+        width: '5%',
+        Cell: RemoveCell,
+        disableSortBy: true
+      }
+    ],
+    [props.data]
   )
+  return <Table<InstanceDetails> data={props.data} bpTableProps={{}} className={css.instanceTable} columns={columns} />
 }

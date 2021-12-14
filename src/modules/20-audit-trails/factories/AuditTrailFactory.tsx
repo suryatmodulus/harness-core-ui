965c884336e612139eb362e2dc2e0f6372bd11c2
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2021-12-14 15:44:08 +0530

    feat: [PL-21102]: Audit trails initial commit

---
+++ src/modules/20-audit-trails/factories/AuditTrailFactory.tsx
@@ -0,0 +1,33 @@
+import type { IconName } from '@wings-software/uicore'
+import type { AuditEventDTO } from 'services/audit'
+
+export type Module = AuditEventDTO['module']
+
+interface Icon {
+  iconName: IconName
+  size?: number
+}
+
+interface AuditTrailModuleHandlerProps {
+  icon: Icon
+  resourceRenderer?: (data: AuditEventDTO) => React.ReactElement
+  eventSummaryRenderer?: () => React.ReactElement
+}
+
+class AuditTrailFactory {
+  private map: Map<Module, AuditTrailModuleHandlerProps>
+
+  constructor() {
+    this.map = new Map()
+  }
+
+  registerHandler(module: Module, props: AuditTrailModuleHandlerProps): void {
+    this.map.set(module, props)
+  }
+
+  getModuleProperties(module: Module): AuditTrailModuleHandlerProps | undefined {
+    return this.map.get(module)
+  }
+}
+
+export default new AuditTrailFactory()

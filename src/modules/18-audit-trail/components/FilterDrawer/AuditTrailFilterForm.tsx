2889d73b688189ae334a5254d798a84f21c42537
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2021-12-31 11:48:19 +0530

    fix: Audit trail filter initial commit

---
+++ src/modules/18-audit-trail/components/FilterDrawer/AuditTrailFilterForm.tsx
@@ -0,0 +1,148 @@
+import { FormInput, MultiSelectOption } from '@wings-software/uicore'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import type { FormikProps } from 'formik'
+import { useMutateAsGet } from '@common/hooks'
+import { StringKeys, useStrings } from 'framework/strings'
+import type { ProjectPathProps } from '@common/interfaces/RouteInterfaces'
+import { useGetUsers, useGetOrganizationAggregateDTOList, useGetProjectListWithMultiOrgFilter } from 'services/cd-ng'
+import { UserItemRenderer, UserTagRenderer } from '@rbac/utils/utils'
+import { actionToLabelMap, getResourceTypeForMultiselect, moduleToLabelMap } from '../../utils/RequestUtil'
+import type { AuditTrailFormType } from './FilterDrawer'
+
+interface AuditTrailFormProps {
+  formikProps?: FormikProps<AuditTrailFormType>
+}
+
+const AuditTrailFilterForm: React.FC<AuditTrailFormProps> = props => {
+  const { accountId, projectIdentifier, orgIdentifier } = useParams<ProjectPathProps>()
+  const [userQuery, setUserQuery] = useState<string>()
+  const [orgQuery, setOrgQuery] = useState<string>()
+  const [projectsQuery, setProjectsQuery] = useState<string>()
+
+  const { data: userData } = useMutateAsGet(useGetUsers, {
+    queryParams: { accountIdentifier: accountId, orgIdentifier, projectIdentifier },
+    body: {
+      searchTerm: userQuery
+    },
+    debounce: 300
+  })
+
+  const { data } = useGetOrganizationAggregateDTOList({
+    queryParams: { accountIdentifier: accountId, searchTerm: orgQuery },
+    debounce: 300
+  })
+
+  const { getString } = useStrings()
+  const users =
+    userData?.data?.content?.map(user => {
+      return {
+        label: user.name || user.email,
+        value: user.email
+      }
+    }) || []
+
+  const organizations =
+    data?.data?.content?.map(org => ({
+      label: org.organizationResponse.organization.name,
+      value: org.organizationResponse.organization.identifier
+    })) || []
+
+  const getOrgs = (): string[] => {
+    if (orgIdentifier) {
+      return [orgIdentifier]
+    }
+
+    return props.formikProps?.values.organizations?.map(org => org.value as string) || []
+  }
+
+  const { data: projectData, refetch: refetchProjectList } = useGetProjectListWithMultiOrgFilter({
+    queryParams: {
+      accountIdentifier: accountId,
+      searchTerm: projectsQuery,
+      orgIdentifiers: getOrgs(),
+      pageSize: 10
+    },
+    queryParamStringifyOptions: {
+      arrayFormat: 'repeat'
+    },
+    debounce: 300
+  })
+
+  const getOptionsForMultiSelect = (map: Record<any, StringKeys>): MultiSelectOption[] => {
+    return Object.keys(map).map(key => ({
+      label: getString(map[key]),
+      value: key
+    }))
+  }
+
+  const projects =
+    projectData?.data?.content?.map(project => ({
+      label: project.project.name,
+      value: project.project.identifier,
+      orgIdentifier: project.project.orgIdentifier
+    })) || []
+
+  return (
+    <>
+      <FormInput.MultiSelect
+        name="users"
+        key="users"
+        items={users}
+        label={getString('common.userLabel')}
+        multiSelectProps={{
+          allowCreatingNewItems: true,
+          onQueryChange: setUserQuery,
+          tagRenderer: UserTagRenderer,
+          itemRender: UserItemRenderer
+        }}
+      />
+      {!orgIdentifier && (
+        <FormInput.MultiSelect
+          name="organizations"
+          key="organizations"
+          items={organizations}
+          label="Org"
+          multiSelectProps={{
+            onQueryChange: setOrgQuery
+          }}
+          onChange={() => {
+            props.formikProps?.setFieldValue('projects', [])
+          }}
+        />
+      )}
+      <FormInput.MultiSelect
+        name="projects"
+        key="projects"
+        label={getString('projectLabel')}
+        items={projects}
+        multiSelectProps={{
+          onQueryChange: query => {
+            setProjectsQuery(query)
+            refetchProjectList()
+          }
+        }}
+      />
+      <FormInput.MultiSelect
+        items={getOptionsForMultiSelect(moduleToLabelMap)}
+        name="modules"
+        label={getString('common.moduleLabel')}
+        key="modules"
+      />
+      <FormInput.MultiSelect
+        items={getResourceTypeForMultiselect()}
+        name="resourceType"
+        label={getString('common.resourceTypeLabel')}
+        key="resourceType"
+      />
+      <FormInput.MultiSelect
+        items={getOptionsForMultiSelect(actionToLabelMap)}
+        name="actions"
+        label={getString('action')}
+        key="actions"
+      />
+    </>
+  )
+}
+
+export default AuditTrailFilterForm

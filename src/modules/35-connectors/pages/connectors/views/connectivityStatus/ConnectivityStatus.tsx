400ae04b9adf18f8da50019995916a6d1831d32b
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2022-01-12 13:30:14 +0530

    fix: Created a separate connectivity status component

--- src/modules/35-connectors/pages/connectors/views/connectivityStatus/ConnectivityStatus.tsx
+++ src/modules/35-connectors/pages/connectors/views/connectivityStatus/ConnectivityStatus.tsx
@@ -7,11 +7,13 @@ import {
   Popover,
   StepsProgress,
   ButtonVariation,
-  ButtonSize
+  ButtonSize,
+  IconName
 } from '@wings-software/uicore'
 import { Position, Intent, PopoverInteractionKind } from '@blueprintjs/core'
 import { useParams } from 'react-router-dom'
 import ReactTimeago from 'react-timeago'
+import type { IconProps } from '@wings-software/uicore/dist/icons/Icon'
 import { useStrings } from 'framework/strings'
 import {
   useGetTestConnectionResult,
@@ -23,7 +25,6 @@ import {
 import { StepIndex, STEP } from '@connectors/common/VerifyOutOfClusterDelegate/VerifyOutOfClusterDelegate'
 import type { StepDetails } from '@connectors/interfaces/ConnectorInterface'
 import { ConnectorStatus } from '@connectors/constants'
-import useTestConnectionErrorModal from '@connectors/common/useTestConnectionErrorModal/useTestConnectionErrorModal'
 import type { ProjectPathProps } from '@common/interfaces/RouteInterfaces'
 import { GetTestConnectionValidationTextByType } from '../../utils/ConnectorUtils'
 import css from '../ConnectorsListView.module.scss'
@@ -49,7 +50,6 @@ const ConnectivityStatus: React.FC<ConnectivityStatusProps> = ({ data }) => {
     status: 'PROCESS' // Replace when enum is added in uikit
   })
 
-  const { openErrorModal } = useTestConnectionErrorModal({})
   const { mutate: reloadTestConnection } = useGetTestConnectionResult({
     identifier: data.connector?.identifier || '',
     queryParams: {
@@ -118,57 +118,49 @@ const ConnectivityStatus: React.FC<ConnectivityStatusProps> = ({ data }) => {
     }
   }, [testing])
 
-  const isStatusSuccess = status === ConnectorStatus.SUCCESS || data.status?.status === ConnectorStatus.SUCCESS
+  const renderStatusText = (
+    icon: IconName,
+    iconProps: Partial<IconProps>,
+    tooltip: JSX.Element | string,
+    statusText: string
+  ): React.ReactElement => {
+    return (
+      <Text
+        inline
+        icon={icon}
+        iconProps={iconProps}
+        tooltip={tooltip}
+        tooltipProps={{ isDark: true, position: 'bottom', popoverClassName: css.tooltip }}
+      >
+        {statusText}
+      </Text>
+    )
+  }
+
+  const connectorStatus = status || data.status?.status
+  const isStatusSuccess = connectorStatus === ConnectorStatus.SUCCESS
 
   return (
     <Layout.Horizontal>
       {!testing ? (
         <Layout.Vertical width="100px">
           <Layout.Horizontal spacing="small">
-            {status || data.status?.status || errorMessage ? (
-              <Text
-                inline
-                icon={isStatusSuccess ? 'full-circle' : 'warning-sign'}
-                iconProps={{
-                  size: isStatusSuccess ? 6 : 12,
-                  color: isStatusSuccess ? Color.GREEN_500 : Color.RED_500
-                }}
-                tooltip={
-                  !isStatusSuccess ? (
-                    errorMessage?.errorSummary || data?.status?.errorSummary ? (
-                      <Layout.Vertical font={{ size: 'small' }} spacing="small" padding="small">
-                        <Text font={{ size: 'small' }} color={Color.WHITE}>
-                          {errorMessage?.errorSummary || data.status?.errorSummary}
-                        </Text>
-                        {errorMessage?.errors || data?.status?.errors ? (
-                          <Text
-                            color={Color.BLUE_400}
-                            onClick={e => {
-                              e.stopPropagation()
-                              openErrorModal((errorMessage as ErrorMessage) || data?.status)
-                            }}
-                            className={css.viewDetails}
-                          >
-                            {getString('connectors.testConnectionStep.errorDetails')}
-                          </Text>
-                        ) : null}
-                      </Layout.Vertical>
-                    ) : (
-                      <Text padding="small" color={Color.WHITE}>
-                        {getString('noDetails')}
-                      </Text>
-                    )
-                  ) : (
-                    ''
+            {errorMessage ||
+              (isStatusSuccess
+                ? renderStatusText(
+                    'full-circle',
+                    { size: 6, color: Color.GREEN_500 },
+                    '',
+                    getString('success').toLowerCase()
                   )
-                }
-                tooltipProps={{ isDark: true, position: 'bottom', popoverClassName: css.tooltip }}
-              >
-                {isStatusSuccess ? getString('success').toLowerCase() : getString('failed').toLowerCase()}
-              </Text>
-            ) : null}
+                : renderStatusText(
+                    'warning-sign',
+                    { size: 12, color: Color.RED_500 },
+                    '',
+                    getString('failed').toLowerCase()
+                  ))}
           </Layout.Horizontal>
-          {status || data.status?.status ? (
+          {connectorStatus ? (
             <Text font={{ size: 'small' }} color={Color.GREY_400}>
               {<ReactTimeago date={lastTestedAt || data.status?.testedAt || ''} />}
             </Text>

ec454870444aee43e944718794c6bd25d37d167f
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2022-01-12 15:13:29 +0530

    fix: Removed duplicate conditions

--- src/modules/35-connectors/pages/connectors/views/connectivityStatus/ConnectivityStatus.tsx
+++ src/modules/35-connectors/pages/connectors/views/connectivityStatus/ConnectivityStatus.tsx
@@ -67,46 +67,44 @@ const ConnectivityStatus: React.FC<ConnectivityStatusProps> = ({ data }) => {
   })
 
   const executeStepVerify = async (): Promise<void> => {
-    if (stepDetails.step === StepIndex.get(STEP.TEST_CONNECTION)) {
-      if (stepDetails.status === 'PROCESS') {
-        try {
-          const result = await reloadTestConnection()
-          setStatus(result?.data?.status)
-          setLastTestedAt(new Date().getTime())
-          if (result?.data?.status === 'SUCCESS') {
-            setStepDetails({
-              step: 2,
-              intent: Intent.SUCCESS,
-              status: 'DONE'
-            })
-          } else {
-            setErrorMessage({ ...result.data, useErrorHandler: false })
-            setStepDetails({
-              step: 1,
-              intent: Intent.DANGER,
-              status: 'ERROR'
-            })
-          }
-          setTesting(false)
-        } catch (err) {
-          setLastTestedAt(new Date().getTime())
-          setStatus('FAILURE')
-          if (err?.data?.responseMessages) {
-            setErrorMessage({
-              errorSummary: err?.data?.message,
-              errors: err?.data?.responseMessages || [],
-              useErrorHandler: true
-            })
-          } else {
-            setErrorMessage({ ...err.message, useErrorHandler: false })
-          }
+    if (stepDetails.step === StepIndex.get(STEP.TEST_CONNECTION) && stepDetails.status === 'PROCESS') {
+      try {
+        const result = await reloadTestConnection()
+        setStatus(result?.data?.status)
+        setLastTestedAt(new Date().getTime())
+        if (result?.data?.status === 'SUCCESS') {
+          setStepDetails({
+            step: 2,
+            intent: Intent.SUCCESS,
+            status: 'DONE'
+          })
+        } else {
+          setErrorMessage({ ...result.data, useErrorHandler: false })
           setStepDetails({
             step: 1,
             intent: Intent.DANGER,
             status: 'ERROR'
           })
-          setTesting(false)
         }
+        setTesting(false)
+      } catch (err) {
+        setLastTestedAt(new Date().getTime())
+        setStatus('FAILURE')
+        if (err?.data?.responseMessages) {
+          setErrorMessage({
+            errorSummary: err?.data?.message,
+            errors: err?.data?.responseMessages || [],
+            useErrorHandler: true
+          })
+        } else {
+          setErrorMessage({ ...err.message, useErrorHandler: false })
+        }
+        setStepDetails({
+          step: 1,
+          intent: Intent.DANGER,
+          status: 'ERROR'
+        })
+        setTesting(false)
       }
     }
   }
@@ -143,29 +141,50 @@ const ConnectivityStatus: React.FC<ConnectivityStatusProps> = ({ data }) => {
   return (
     <Layout.Horizontal>
       {!testing ? (
-        <Layout.Vertical width="100px">
-          <Layout.Horizontal spacing="small">
-            {errorMessage ||
-              (isStatusSuccess
-                ? renderStatusText(
-                    'full-circle',
-                    { size: 6, color: Color.GREEN_500 },
-                    '',
-                    getString('success').toLowerCase()
-                  )
-                : renderStatusText(
-                    'warning-sign',
-                    { size: 12, color: Color.RED_500 },
-                    '',
-                    getString('failed').toLowerCase()
-                  ))}
-          </Layout.Horizontal>
-          {connectorStatus ? (
-            <Text font={{ size: 'small' }} color={Color.GREY_400}>
-              {<ReactTimeago date={lastTestedAt || data.status?.testedAt || ''} />}
-            </Text>
-          ) : null}
-        </Layout.Vertical>
+        <>
+          <Layout.Vertical width="100px">
+            <Layout.Horizontal spacing="small">
+              {errorMessage
+                ? isStatusSuccess
+                  ? renderStatusText(
+                      'full-circle',
+                      { size: 6, color: Color.GREEN_500 },
+                      '',
+                      getString('success').toLowerCase()
+                    )
+                  : renderStatusText(
+                      'warning-sign',
+                      { size: 12, color: Color.RED_500 },
+                      '',
+                      getString('failed').toLowerCase()
+                    )
+                : undefined}
+            </Layout.Horizontal>
+            {connectorStatus ? (
+              <Text font={{ size: 'small' }} color={Color.GREY_400}>
+                {<ReactTimeago date={lastTestedAt || data.status?.testedAt || ''} />}
+              </Text>
+            ) : null}
+          </Layout.Vertical>
+          {!isStatusSuccess ? (
+            <Button
+              variation={ButtonVariation.SECONDARY}
+              size={ButtonSize.SMALL}
+              text={getString('test')}
+              className={css.testBtn}
+              onClick={e => {
+                e.stopPropagation()
+                setTesting(true)
+                setStepDetails({
+                  step: 1,
+                  intent: Intent.WARNING,
+                  status: 'PROCESS' // Replace when enum is added in uikit
+                })
+              }}
+              withoutBoxShadow
+            />
+          ) : undefined}
+        </>
       ) : (
         <Layout.Horizontal>
           <Popover interactionKind={PopoverInteractionKind.HOVER} position={Position.LEFT_TOP}>
@@ -182,24 +201,6 @@ const ConnectivityStatus: React.FC<ConnectivityStatusProps> = ({ data }) => {
           <Text style={{ margin: 8 }}>{getString('connectors.testInProgress')}</Text>
         </Layout.Horizontal>
       )}
-      {!testing && !isStatusSuccess ? (
-        <Button
-          variation={ButtonVariation.SECONDARY}
-          size={ButtonSize.SMALL}
-          text={getString('test')}
-          className={css.testBtn}
-          onClick={e => {
-            e.stopPropagation()
-            setTesting(true)
-            setStepDetails({
-              step: 1,
-              intent: Intent.WARNING,
-              status: 'PROCESS' // Replace when enum is added in uikit
-            })
-          }}
-          withoutBoxShadow
-        />
-      ) : null}
     </Layout.Horizontal>
   )
 }

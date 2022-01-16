e96439459a81fdd9b9aa5636c8809cb7912d076f
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2022-01-16 22:29:05 +0530

    fix: [PL-20872]: Fix search and pagination

--- src/modules/10-common/components/EntityReference/EntityReference.tsx
+++ src/modules/10-common/components/EntityReference/EntityReference.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import cx from 'classnames'
 import {
   Container,
@@ -142,7 +142,7 @@ export function EntityReference<T>(props: EntityReferenceProps<T>): JSX.Element 
 
   const delayedFetchRecords = useRef(debounce((fn: () => void) => fn(), 300)).current
 
-  const fetchData = useCallback(() => {
+  const fetchData = (searchTearmChange: boolean) => {
     try {
       setError(null)
       if (!searchTerm) {
@@ -152,9 +152,9 @@ export function EntityReference<T>(props: EntityReferenceProps<T>): JSX.Element 
           setLoading(false)
         })
       } else {
-        // if (props.pagination.pageIndex !== 0) {
-        //   props.pagination.gotoPage?.(0)
-        // }
+        if (searchTearmChange && props.pagination.pageIndex !== 0) {
+          props.pagination.gotoPage?.(0)
+        }
         delayedFetchRecords(() => {
           setLoading(true)
           setSelectedRecord(undefined)
@@ -167,11 +167,15 @@ export function EntityReference<T>(props: EntityReferenceProps<T>): JSX.Element 
     } catch (msg) {
       setError(msg)
     }
-  }, [props.pagination.pageIndex, selectedScope, delayedFetchRecords, searchTerm, searchInlineComponent])
+  }
 
   useEffect(() => {
-    fetchData()
-  }, [fetchData])
+    fetchData(true)
+  }, [selectedScope, delayedFetchRecords, searchTerm, searchInlineComponent])
+
+  useEffect(() => {
+    fetchData(false)
+  }, [props.pagination.pageIndex])
 
   const onScopeChange = (scope: Scope): void => {
     setSelectedRecord(undefined)
@@ -210,7 +214,7 @@ export function EntityReference<T>(props: EntityReferenceProps<T>): JSX.Element 
           </Container>
         ) : error ? (
           <Container>
-            <PageError message={error} onClick={fetchData} />
+            <PageError message={error} onClick={() => fetchData(true)} />
           </Container>
         ) : data.length ? (
           <CollapsableList<T>

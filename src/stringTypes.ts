236c8c8848b8e9b9e1112b55d33e8785eea31701
Samarth Sharma <samarth.sharma@harness.io>
2022-01-17 14:17:47 +0530

    fix: [PIE-2091]: added handling for lengthy text spillover in input set list and form view

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -2410,6 +2410,7 @@ export interface StringsMap {
   'pipeline.inputSets.applyInputSets': string
   'pipeline.inputSets.applyingInputSet': string
   'pipeline.inputSets.applyingInputSets': string
+  'pipeline.inputSets.inputSetNameLabel': string
   'pipeline.inputSets.inputSetPlaceholder': string
   'pipeline.inputSets.noRuntimeInputsCurrently': string
   'pipeline.inputSets.noRuntimeInputsWhileExecution': string

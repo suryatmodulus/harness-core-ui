e92aa289b0f3266aa828ec406bf975832903edfc
Vivek Kumar Bansal <vivek.bansal@harness.io>
2022-01-19 11:52:23 +0530

    fix: [PL-22040]: update routes to parse module param

--- src/modules/10-common/__tests__/RouteDefinitions.test.tsx
+++ src/modules/10-common/__tests__/RouteDefinitions.test.tsx
@@ -3,9 +3,10 @@ import routes from '../RouteDefinitions'
 const MODULES = ['ci', 'cd', 'ce', 'cf', 'cv']
 
 describe('RouteDefinitions tests', () => {
+  // eslint-disable-next-line jest/expect-expect
   test('module name is not hardcoded', () => {
     Object.values(routes).forEach(fn => {
-      const path = fn({})
+      const path = fn({} as any)
 
       MODULES.forEach(m => {
         if (path.includes(`/${m}`)) {
